791529a7985bd9e92228ba9b256bccb242aca368
FredeBr <fredebrx@gmail.com>
2023-09-05 08:55:57 -0400

    Comunicação Síncrona - Preparação de Deleção Entre Microservices

--- src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Service
 public class UtilsServiceImpl implements UtilsService {
     @Override
-    public String createUrl(UUID courseId, Pageable pageable) {
+    public String createUrlGetAllUsersByCourse(UUID courseId, Pageable pageable) {
         return "/users?courseId=" + courseId + "&page=" + pageable.getPageNumber() + "&size="
                 + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
     }
